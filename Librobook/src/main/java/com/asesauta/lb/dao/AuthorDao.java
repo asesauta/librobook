0c9f1efff4e08a03f0b9c3fdaaeb71411bb29a50
asesauta <jorgetercero@gmail.com>
2013-02-05 00:14:30 +0100

    hib

--- Librobook/src/main/java/com/asesauta/lb/dao/AuthorDao.java
+++ Librobook/src/main/java/com/asesauta/lb/dao/AuthorDao.java
@@ -5,6 +5,7 @@ import javax.annotation.Resource;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.asesauta.lb.data.Author;
 
